94a748d8af9e75360480166f5ee6c99987dfba05
unknown <kaku@LAPF0ZDGQEA.kmjp.local>
2018-10-02 19:56:20 +0900

    unixtime to hmac hex digest.

--- hotp.py
+++ hotp.py
@@ -1,8 +1,16 @@
 import datetime
 import time
+import hmac
+import hashlib
 
 now = datetime.datetime.now()
 print(now)
 
-unix = int(time.mktime(now.timetuple()))
-print(unix)
+unix_time = int(time.mktime(now.timetuple()))
+print(unix_time)
+
+key = bytes("secret key", 'ascii')
+text = bytes(str(unix_time), 'ascii')
+dig = hmac.new(key, text, hashlib.sha1)
+print(dig.hexdigest())
+
