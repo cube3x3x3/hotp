601cf497764e2245865f5bbbee6a09893644201d
cube3x3x3 <cube3x3x3@gmail.com>
2018-10-04 22:41:56 +0900

    refactoring

--- Test_hotp.py
+++ Test_hotp.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 # logging.getLogger('').setLevel(logging.INFO)
 
-class TestHOTP(unittest.TestCase):
+class Test_hotp(unittest.TestCase):
 
     def test_rfc4226_test_case(self):
         _secret = "12345678901234567890"
@@ -60,23 +60,6 @@ class TestHOTP(unittest.TestCase):
         # 5.4.  Example of HOTP Computation for Digit = 6
         example_value = codecs.decode(b'1f8698690e02ca16618550ef7f19da8e945b555a', 'hex_codec')
         self.assertEqual('872921', hotp.truncate(example_value))
-
-    def test_create_HOTP(self):
-        timestep = 30
-        message_time= hotp.create_messsage_time(30)
-        logger.info('messega_time %s', message_time)
-        t_key = bytes("secret key", 'ascii')
-        t_text = bytes(str(message_time), 'ascii')
-        t_digest = hotp.create_hmacdigt(t_key, t_text)
-        self.assertTrue(hotp.HOTP_Computation(t_digest))
-
-    def test_fixedtime_HOTP(self):
-        fixed_message_time = int(51282858)
-        logger.info('fixed_message_time:%s', fixed_message_time)
-        f_key = bytes("secret key", 'ascii')
-        f_text = bytes(str(fixed_message_time), 'ascii')
-        f_bindig = hotp.create_hmacdigt(f_key, f_text)
-        self.assertEqual('811807', hotp.HOTP_Computation(f_bindig))
 
 
 if __name__ == "__main__":
